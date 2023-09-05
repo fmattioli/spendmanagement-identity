730c85ce1df3da2e4d79d2eb42209fb2e5a4cccb
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2023-09-06 00:35:21 +0100

    FEAT: Adding new swagger authorize button

--- src/SpendManagement.Identity.API/Program.cs
+++ src/SpendManagement.Identity.API/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using SpendManagement.Identity.IoC.Extensions;
+using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,11 +12,38 @@ builder.Services.AddCors();
 builder.Services.AddControllers().AddJsonOptions(options =>
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo{ Title = "SpendManagement.Identity.API", Version = "v1", Description = "The users management related to the SpendManagement project." }));
-
 builder.Services.AddAuthentication(builder.Configuration);
 builder.Services.AddAuthorizationPolicies();
 builder.Services.RegisterServices(builder.Configuration);
+
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "SpendManagement.Identity", Version = "v1", Description = "The users management related to the SpendManagement project." });
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please insert token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer"
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+    });
 
 var app = builder.Build();
 
