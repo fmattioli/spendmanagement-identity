02d13b93de4a4b231c8640ca661b08ae929ee226
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2023-09-07 15:09:19 +0100

    FEAT: Adding health checkers

---
+++ src/SpendManagement.Identity.IoC/Extensions/HealthCheckExtensions.cs
@@ -0,0 +1,31 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SpendManagement.Identity.IoC.Extensions
+{
+    public static class HealthCheckExtensions
+    {
+        public static void AddHealthCheckers(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("SpendManagementIdentity"), name: "SqlServer", tags: new string[] { "db", "data" });
+
+            services.AddHealthChecksUI()
+                .AddInMemoryStorage();
+        }
+
+        public static void UseHealthCheckers(this IApplicationBuilder app)
+        {
+            app.UseHealthChecks("/health", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecksUI(options => options.UIPath = "/monitor");
+        }
+    }
+}
---
+++ src/SpendManagement.Identity.IoC/Extensions/SwaggarExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace SpendManagement.Identity.IoC.Extensions
+{
+    public static class SwaggarExtensions
+    {
+        public static void AddSwaggerExtensions(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSwaggerGen(c =>
+             {
+                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SpendManagement.Identity", Version = "v1", Description = "The users management related to the SpendManagement project." });
+                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                 {
+                     In = ParameterLocation.Header,
+                     Description = "Please insert token",
+                     Name = "Authorization",
+                     Type = SecuritySchemeType.Http,
+                     Scheme = "Bearer"
+                 });
+                 c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                 {
+                     {
+                         new OpenApiSecurityScheme
+                         {
+                             Reference = new OpenApiReference
+                             {
+                                 Type = ReferenceType.SecurityScheme,
+                                 Id = "Bearer"
+                             }
+                         },
+                         Array.Empty<string>()
+                     }
+                 });
+             });
+        }
+    }
+}
