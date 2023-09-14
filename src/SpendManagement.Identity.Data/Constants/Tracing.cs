ea2eb74d7040471f64d461f1a9a47eb011e2b45e
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2023-09-14 19:31:51 +0100

    FEAT: Adding tracing for requests

---
+++ src/SpendManagement.Identity.Data/Constants/Tracing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendManagement.Identity.Data.Constants
+{
+    public static class Tracing
+    {
+        public const string ApplicationName = "SpendManagement.Identity";
+    }
+}
---
+++ src/SpendManagement.Identity.IoC/Extensions/TracingExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using SpendManagement.Identity.Data.Constants;
+
+namespace SpendManagement.Identity.IoC.Extensions
+{
+    public static class TracingExtensions
+    {
+        public static IServiceCollection AddTracing(this IServiceCollection services)
+        {
+            services.AddOpenTelemetry().WithTracing(tcb =>
+             {
+                 tcb
+                 .AddSource(Tracing.ApplicationName)
+                 .SetResourceBuilder(
+                     ResourceBuilder.CreateDefault()
+                         .AddService(serviceName: Tracing.ApplicationName))
+                 .AddSqlClientInstrumentation(options => options.SetDbStatementForText = true)
+                 .AddAspNetCoreInstrumentation()
+                 .AddOtlpExporter();
+             });
+
+            services.AddSingleton(TracerProvider.Default.GetTracer(Tracing.ApplicationName));
+
+            return services;
+        }
+    }
+}
