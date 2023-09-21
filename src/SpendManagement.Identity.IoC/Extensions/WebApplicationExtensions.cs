cf96ddb8bc969898109235c25ddea208c89cd9cf
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2023-09-21 14:25:30 +0100

    FEAT: Adding docker support to application

---
+++ src/SpendManagement.Identity.IoC/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SpendManagement.Identity.Data.Data;
+
+namespace SpendManagement.Identity.IoC.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static void RunMigrationsOnApplicationStart(this WebApplication webApplication)
+        {
+            using (var scope = webApplication.Services.CreateScope())
+            {
+                var dataContext = scope.ServiceProvider.GetRequiredService<IdentityDataContext>();
+                dataContext.Database.Migrate();
+            }
+        }
+    }
+}
